741b2716c35ed238dee003b78fa00d349f4742be
admin <admin@admin.com>
2021-06-14 19:17:58 +0430

    add requirements

--- web/models.py
+++ web/models.py
@@ -8,8 +8,15 @@ class Expense(models.Model):
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.text} | {self.amount} | {self.date}"
+
+
 class Income(models.Model):
     text = models.TextField()
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.text} | {self.amount} | {self.date}"
